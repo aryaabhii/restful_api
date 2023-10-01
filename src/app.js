08873f5fb6eb965795f94d2c1a68084b4705963a
Abhijeet Kumar <you@example.comaryaabhi84@gmail.com>
2023-10-01 18:56:53 +0530

    abhi_changes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const app = express();
+const app  = express();
 const port = process.env.PORT || 8080;
 app.use(express.json()); // middleware
 
 require("./database/connection");
 const Student = require("./models/user");
-
 
 // client will request
 // server give response on the basis of client
@@ -22,7 +21,6 @@ app.post("/student", (req, res) => {
         res.send(error);
     });
 });
-
 
 app.listen(port, () => {
     console.log(`Server started at ${port}`);
